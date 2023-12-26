7631320efda501e2b3d0a6c2e665b9b3caef7b3a
Kristof Bach <crys@crys.hu>
2023-12-26 14:48:57 +0100

    Implement toggle actions and left/right separate controller for Legrand switches

--- apps/controllerx/cx_devices/legrand.py
+++ apps/controllerx/cx_devices/legrand.py
@@ -20,6 +20,7 @@ def get_zha_action_LegrandWallController(data: dict[str, Any]) -> str:
 class Legrand600083LightController(LightController):
     def get_z2m_actions_mapping(self) -> DefaultActionsMapping:
         return {
+            "toggle": Light.TOGGLE,
             "on": Light.ON,
             "off": Light.OFF,
             "brightness_move_up": Light.HOLD_BRIGHTNESS_UP,
@@ -29,6 +30,7 @@ class Legrand600083LightController(LightController):
 
     def get_zha_actions_mapping(self) -> DefaultActionsMapping:
         return {
+            "1_toggle": Light.TOGGLE,
             "1_on": Light.ON,
             "1_off": Light.OFF,
             "1_move_up": Light.HOLD_BRIGHTNESS_UP,
@@ -43,6 +45,7 @@ class Legrand600083LightController(LightController):
 class Legrand600083Z2MLightController(Z2MLightController):
     def get_z2m_actions_mapping(self) -> DefaultActionsMapping:
         return {
+            "toggle": Z2MLight.TOGGLE,
             "on": Z2MLight.ON,
             "off": Z2MLight.OFF,
             "brightness_move_up": Z2MLight.HOLD_BRIGHTNESS_UP,
@@ -54,11 +57,13 @@ class Legrand600083Z2MLightController(Z2MLightController):
 class Legrand600088LightController(LightController):
     def get_z2m_actions_mapping(self) -> DefaultActionsMapping:
         return {
+            "toggle_left": Light.TOGGLE,
             "on_left": Light.ON,
             "off_left": Light.OFF,
             "brightness_move_up_left": Light.HOLD_COLOR_UP,
             "brightness_move_down_left": Light.HOLD_COLOR_DOWN,
             "brightness_stop_left": Light.RELEASE,
+            "toggle_right": Light.TOGGLE_FULL_BRIGHTNESS,
             "on_right": Light.ON_FULL_BRIGHTNESS,
             "off_right": Light.ON_MIN_BRIGHTNESS,
             "brightness_move_up_right": Light.HOLD_BRIGHTNESS_UP,
@@ -68,11 +73,13 @@ class Legrand600088LightController(LightController):
 
     def get_zha_actions_mapping(self) -> DefaultActionsMapping:
         return {
+            "1_toggle": Light.TOGGLE,
             "1_on": Light.ON,
             "1_off": Light.OFF,
             "1_move_up": Light.HOLD_COLOR_UP,
             "1_move_down": Light.HOLD_COLOR_DOWN,
             "1_stop": Light.RELEASE,
+            "2_toggle": Light.TOGGLE_FULL_BRIGHTNESS,
             "2_on": Light.ON_FULL_BRIGHTNESS,
             "2_off": Light.ON_MIN_BRIGHTNESS,
             "2_move_up": Light.HOLD_BRIGHTNESS_UP,
@@ -84,16 +91,92 @@ class Legrand600088LightController(LightController):
         return get_zha_action_LegrandWallController(data)
 
 
+class Legrand600088LeftLightController(LightController):
+    def get_z2m_actions_mapping(self) -> DefaultActionsMapping:
+        return {
+            "toggle_left": Light.TOGGLE,
+            "on_left": Light.ON,
+            "off_left": Light.OFF,
+            "brightness_move_up_left": Light.HOLD_COLOR_UP,
+            "brightness_move_down_left": Light.HOLD_COLOR_DOWN,
+            "brightness_stop_left": Light.RELEASE,
+        }
+
+    def get_zha_actions_mapping(self) -> DefaultActionsMapping:
+        return {
+            "1_toggle": Light.TOGGLE,
+            "1_on": Light.ON,
+            "1_off": Light.OFF,
+            "1_move_up": Light.HOLD_COLOR_UP,
+            "1_move_down": Light.HOLD_COLOR_DOWN,
+            "1_stop": Light.RELEASE,
+        }
+
+    def get_zha_action(self, data: EventData) -> str:
+        return get_zha_action_LegrandWallController(data)
+
+
+class Legrand600088RightLightController(LightController):
+    def get_z2m_actions_mapping(self) -> DefaultActionsMapping:
+        return {
+            "toggle_right": Light.TOGGLE,
+            "on_right": Light.ON,
+            "off_right": Light.OFF,
+            "brightness_move_up_right": Light.HOLD_COLOR_UP,
+            "brightness_move_down_right": Light.HOLD_COLOR_DOWN,
+            "brightness_stop_right": Light.RELEASE,
+        }
+
+    def get_zha_actions_mapping(self) -> DefaultActionsMapping:
+        return {
+            "2_toggle": Light.TOGGLE,
+            "2_on": Light.ON,
+            "2_off": Light.OFF,
+            "2_move_up": Light.HOLD_COLOR_UP,
+            "2_move_down": Light.HOLD_COLOR_DOWN,
+            "2_stop": Light.RELEASE,
+        }
+
+    def get_zha_action(self, data: EventData) -> str:
+        return get_zha_action_LegrandWallController(data)
+
+
 class Legrand600088Z2MLightController(Z2MLightController):
     def get_z2m_actions_mapping(self) -> DefaultActionsMapping:
         return {
+            "toggle_left": Z2MLight.TOGGLE,
             "on_left": Z2MLight.ON,
             "off_left": Z2MLight.OFF,
             "brightness_move_up_left": Z2MLight.HOLD_COLOR_TEMP_UP,
             "brightness_move_down_left": Z2MLight.HOLD_COLOR_TEMP_DOWN,
             "brightness_stop_left": Z2MLight.RELEASE,
+            "toggle_right": Z2MLight.TOGGLE_FULL_BRIGHTNESS,
             "on_right": Z2MLight.ON_FULL_BRIGHTNESS,
             "off_right": Z2MLight.ON_MIN_BRIGHTNESS,
+            "brightness_move_up_right": Z2MLight.HOLD_BRIGHTNESS_UP,
+            "brightness_move_down_right": Z2MLight.HOLD_BRIGHTNESS_DOWN,
+            "brightness_stop_right": Z2MLight.RELEASE,
+        }
+
+
+class Legrand600088LeftZ2MLightController(Z2MLightController):
+    def get_z2m_actions_mapping(self) -> DefaultActionsMapping:
+        return {
+            "toggle_left": Z2MLight.TOGGLE,
+            "on_left": Z2MLight.ON,
+            "off_left": Z2MLight.OFF,
+            "brightness_move_up_left": Z2MLight.HOLD_BRIGHTNESS_UP,
+            "brightness_move_down_left": Z2MLight.HOLD_BRIGHTNESS_DOWN,
+            "brightness_stop_left": Z2MLight.RELEASE,
+        }
+
+
+class Legrand600088RightZ2MLightController(Z2MLightController):
+    def get_z2m_actions_mapping(self) -> DefaultActionsMapping:
+        return {
+            "toggle_right": Z2MLight.TOGGLE,
+            "on_right": Z2MLight.ON,
+            "off_right": Z2MLight.OFF,
             "brightness_move_up_right": Z2MLight.HOLD_BRIGHTNESS_UP,
             "brightness_move_down_right": Z2MLight.HOLD_BRIGHTNESS_DOWN,
             "brightness_stop_right": Z2MLight.RELEASE,
